de83b73ae3e12076f520e13b4867044fc804b6e6
a569433273 <569433273@qq.com>
2014-07-30 09:55:06 +0800

    修改了占座证件号少一位的问题

--- src/com/liu/baobiao/action/zhanzuo.java
+++ src/com/liu/baobiao/action/zhanzuo.java
@@ -72,7 +72,7 @@ public class zhanzuo extends ActionSupport implements ModelDriven<Object> {
 				nmhang.trim();
 				ssrhang = ssrhang + "ssr foid "
 						+ hangban.get(0).substring(0, 2) + " hk/ni"
-						+ as[3].substring(1, as[3].length()) + "/p" + (i + 1)
+						+ as[3].substring(0, as[3].length()) + "/p" + (i + 1)
 						+ "\n";
 			}
 
